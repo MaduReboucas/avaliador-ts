fc7daaa9423a5cec7ae4f31fbbc28c2931a3c781
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-01 19:31:42 -0300

    inciando avaliação

--- src/components/ReviewForm.tsx
+++ src/components/ReviewForm.tsx
@@ -1,7 +1,47 @@
 import "./ReviewForm.css";
 
-const ReviewForm = () => {
-  return <div>ReviewForm</div>;
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+type ReviewForm = {
+  data: {
+    review: string;
+    comment: string;
+  };
+  updateFieldHandler: (key: string, value: string) => void;
+};
+
+const ReviewForm = ({ data, updateFieldHandler }: ReviewForm) => {
+  return (
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" name="review" value="unsatisfied" required />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" name="review" value="neutral" required />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" name="review" value="satisfied" required />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" name="review" value="very_satisfied" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito Satisfeito</p>
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewForm;
